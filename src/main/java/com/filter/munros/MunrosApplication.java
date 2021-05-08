c84226be51e508b47c966e060f6f963f16a3b4a7
din06 <daniel.inglis@sky.uk>
2021-05-08 11:09:13 +0100

    djames| spring boot init, project setup|

---
+++ src/main/java/com/filter/munros/MunrosApplication.java
@@ -0,0 +1,13 @@
+package com.filter.munros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filter/munros/MunrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filter.munros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
